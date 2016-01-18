635483aba61fbb4452d4795bcc0eb440524a52f9
cgoss <colin.goss@gmail.com>
2016-01-17 17:15:51 -0800

    Added methods to call list user service.

--- js/usermanagement.js
+++ js/usermanagement.js
@@ -8,6 +8,74 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('UsermanagementCtrl', function () {
-    
+  .controller('UsermanagementCtrl', function (
+    $location,
+    $scope,
+    $http
+  ) {
+      servicelocation = "https://" + $location.$$host + "/api/";
+
+    $scope.checkServiceStatus = function() {
+        console.log("Interval occurred check status");
+        var data = "servicename=elasticsearch";
+        var config = {headers:{
+          "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+        }};
+        $http.post(servicelocation+"IsServiceRunning",data,config)
+          .success(function(data)
+            {
+               $scope.systemsettings.elasticsearchstatusbit=data;
+            });
+         data = "servicename=logstash";
+        $http.post(servicelocation+"IsServiceRunning",data,config)
+          .success(function(data)
+            {
+               $scope.systemsettings.logstashstatusbit=data;
+            });
+         data = "servicename=kibana4";
+        $http.post(servicelocation+"IsServiceRunning",data,config)
+          .success(function(data)
+            {
+               $scope.systemsettings.kibanastatusbit=data;
+            });
+    };
+
+    $scope.listUsers = function(){
+      console.log("Get List of users");
+      var data = "";
+      var config = {headers:{
+        "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+      }};
+      $http.post(servicelocation+"ListUsers",data,config)
+        .success(function(data)
+          {
+             $scope.userList=data;
+          });
+    };
+
+    $scope.updateUser = function(user,password){
+      console.log("Get List of users");
+      var data = "User=" +user + ";Password=" + password;
+      var config = {headers:{
+        "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+      }};
+      $http.post(servicelocation+"UpdateUser",data,config)
+        .success(function(data)
+          {
+             $scope.listUsers();
+          });
+    };
+
+    $scope.deleteUsers = function(user){
+      console.log("Get List of users");
+      var data = "User=" + user;
+      var config = {headers:{
+        "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+      }};
+      $http.post(servicelocation+"DeleteUser",data,config)
+        .success(function(data)
+          {
+             $scope.listUsers();
+          });
+    };
   });
